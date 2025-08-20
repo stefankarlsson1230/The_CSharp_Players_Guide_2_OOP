e9a8712b7d56f65c42c4934690029675e5c0af50
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-20 15:47:38 +0200

    Finished Project_17_4

---
+++ Project_17_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Rectangle Info - Return area and perimeter as tuple.
+
+double length, width;
+
+Console.Write("Length: ");
+length = double.Parse(Console.ReadLine());
+
+Console.Write("Width: ");
+width = double.Parse(Console.ReadLine());
+
+
+(double area, double perimeter) = GetRectangleInfo(length, width);
+
+Console.WriteLine($"Area: {area}");
+Console.WriteLine($"Perimeter: {perimeter}");
+
+
+// Local Functions
+(double area, double perimeter) GetRectangleInfo(double length, double width) => (length *  width, length * 2 + width * 2);
+
