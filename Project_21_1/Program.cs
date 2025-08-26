b9149648f28e1a0e5c7f05c781bbd1c67f8e46db
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-08-26 13:59:26 +0200

    Finished Project_21_1

---
+++ Project_21_1/Program.cs
@@ -0,0 +1,138 @@
+﻿Arrow arrow;
+int input = -1;
+
+while (input < 1 || input > 2)
+{
+    Console.Write("Do you want 1. a standard arrow or 2. a custom made arrow? ");
+    input = int.Parse(Console.ReadLine());
+}
+
+if (input == 1)
+{
+    input = -1;
+
+    while (input < 1 || input > 3)
+    {
+        Console.Write("Choose arrowtype (1. Elite Arrow, 2. Beginner Arrow, 3. Marksman Arrow): ");
+        input = int.Parse(Console.ReadLine());
+    }
+
+    arrow = input switch
+    {
+        1 => Arrow.CreateEliteArrow(),
+        2 => Arrow.CreateBeginnerArrow(),
+        3 => Arrow.CreateMarksmanArrow(),
+    };
+}
+else
+{
+
+    arrow = new Arrow();
+    input = -1;
+
+    while (input < 1 || input > 3)
+    {
+        Console.Write("Choose arrowhead (1. Steel, 2. Wood, 3. Obsidian): ");
+        input = int.Parse(Console.ReadLine());
+    }
+
+    switch (input)
+    {
+        case 1:
+            arrow.Arrowhead = Arrowhead.Steel;
+            break;
+        case 2:
+            arrow.Arrowhead = Arrowhead.Wood;
+            break;
+        case 3:
+            arrow.Arrowhead = Arrowhead.Obsidian;
+            break;
+    }
+
+    input = -1;
+
+    while (input < 1 || input > 3)
+    {
+        Console.Write("Choose fletching (1. Plastic, 2. Turkey, 3. Goose): ");
+        input = int.Parse(Console.ReadLine());
+    }
+
+    switch (input)
+    {
+        case 1:
+            arrow.Fletching = Fletching.Plastic;
+            break;
+        case 2:
+            arrow.Fletching = Fletching.Turkey;
+            break;
+        case 3:
+            arrow.Fletching = Fletching.Goose;
+            break;
+    }
+
+
+    while (input < 60 || input > 100)
+    {
+        Console.Write("Choose shaft-legth (60 - 100 cm): ");
+        input = int.Parse(Console.ReadLine());
+    }
+
+    arrow.Shaft = input;
+}
+
+Console.WriteLine($"Your {arrow.Shaft} cm long arrow, with a {arrow.Arrowhead} arrowhead and {arrow.Fletching} fletching will cost you {arrow.GetCost():0.00} gold");
+
+
+
+// Classes
+class Arrow
+{
+    // Properties
+    public Arrowhead Arrowhead { get; set; }
+    public Fletching Fletching { get; set; }
+    public int Shaft { get; set; }
+
+
+    // Constructors
+    public Arrow() { }
+
+    public Arrow(Arrowhead arrowhead, Fletching fletching, int shaft)
+    {
+        Arrowhead = arrowhead;
+        Fletching = fletching;
+        Shaft = shaft;
+    }
+
+
+    // Methods
+    public static Arrow CreateEliteArrow() => new Arrow(Arrowhead.Steel, Fletching.Plastic, 95);
+    public static Arrow CreateBeginnerArrow() => new Arrow(Arrowhead.Wood, Fletching.Goose, 75);
+    public static Arrow CreateMarksmanArrow() => new Arrow(Arrowhead.Steel, Fletching.Goose, 65);
+
+    public float GetCost()
+    {
+        float price = Arrowhead switch
+        {
+            Arrowhead.Steel => 10,
+            Arrowhead.Wood => 3,
+            Arrowhead.Obsidian => 5,
+        };
+
+        price += Fletching switch
+        {
+            Fletching.Plastic => 10,
+            Fletching.Turkey => 5,
+            Fletching.Goose => 3,
+        };
+
+        price += Shaft * 0.05f;
+
+        return price;
+    }
+}
+
+
+
+// Enums
+enum Arrowhead { Steel, Wood, Obsidian }
+enum Fletching { Plastic, Turkey, Goose }
--- README.md
+++ README.md
@@ -104,13 +104,25 @@ to display the arrow’s cost.
   - Display All Properties – Loop and print them.
 
 
+## Chapter 21: Static
 
-Chapter 21: The Catacombs of the Class
-    1. Create Point – With X and Y fields.
-    2. Create Color – RGB class and ToHex() method.
-    3. Create Card – Enum for suit, rank.
-    4. Lockable Door – With Unlock() and password.
-    5. Password Validator – Check password rules.
+### From The Book
+- [x] Project 1 - Arrow Factories
+  - Vin Fletcher sometimes makes custom-ordered arrows, but these are rare. Most of the time, he sells one
+of the following standard arrows:
+    - The Elite Arrow, made from a steel arrowhead, plastic fletching, and a 95 cm shaft.
+    - The Beginner Arrow, made from a wood arrowhead, goose feathers, and a 75 cm shaft.
+    - The Marksman Arrow, made from a steel arrowhead, goose feathers, and a 65 cm shaft.
+- You can make static methods to make these specific variations of arrows easy.
+- Objectives:
+  - Modify your Arrow class one final time to include static methods of the form public static
+Arrow CreateEliteArrow() { ... } for each of the three above arrow types.
+  - Modify the program to allow users to choose one of these pre-defined types or a custom arrow. If
+they select one of the predefined styles, produce an Arrow instance using one of the new static
+methods. If they choose to make a custom arrow, use your earlier code to get their custom data
+about the desired arrow.
+
+
 
 Chapter 22: Null References
     1. Null Name Check – Warn if name is null.
