dc641c3860c5f3cd60e43400090fa3fda577fa51
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-09-06 18:31:47 +0200

    Finished Project_32_3

---
+++ Project_32_3/Program.cs
@@ -0,0 +1,114 @@
+﻿Robot robot = new Robot();
+string? input;
+
+Console.WriteLine("Enter commands (on, off, north, south, west, east) end with \"stop\"");
+do
+{
+    Console.Write($"Command: ");
+    input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "on":
+            robot.Commands.Add(new OnCommand());
+            break;
+        case "off":
+            robot.Commands.Add(new OffCommand());
+            break;
+        case "north":
+            robot.Commands.Add(new NorthCommand());
+            break;
+        case "south":
+            robot.Commands.Add(new SouthCommand());
+            break;
+        case "east":
+            robot.Commands.Add(new EastCommand());
+            break;
+        case "west":
+            robot.Commands.Add(new WestCommand());
+            break;
+        default:
+            break;
+    }
+}
+while (input != "stop");
+
+
+robot.Run();
+
+
+
+// Classes
+public class Robot
+{
+    // Properties
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public List<IRobotCommand?> Commands { get; } = new List<IRobotCommand?>();
+
+    // Methods
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y++;
+    }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y--;
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X--;
+    }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X++;
+    }
+}
--- README.md
+++ README.md
@@ -586,6 +586,15 @@ player spent exploring the caverns.
     - When a game finishes (win or loss), capture the current time.
     - Use TimeSpan to compute how much time elapsed and display that to the player.
 
+- [x] Project 3 - List of Commands
+  - In Level 27, we encountered a robot with an array to hold commands to run. But we could make the robot
+have as many commands as we want by turning the array into a list. Revisit that challenge to make the
+robot use a list instead of an array, and add commands to run until the user says to stop.
+  - Objectives: 
+    - Change the Robot class to use a List`<IRobotCommand>` instead of an array for its Commands
+property.
+    - Instead of looping three times, go until the user types stop. Then run all of the commands created.
+
 ### From ChatGPT
     1. Dice Roller – Use Random class.
     2. Date Difference – Use DateTime to count days.
